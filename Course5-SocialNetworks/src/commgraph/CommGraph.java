c2bb3a086cc22c913d10b2edf522c42c3d24fe03
Ginny-Dang <ginnydang0728@gmail.com>
2022-08-11 20:18:22 -0500

    Rename Course projects

---
+++ Course5-SocialNetworks/src/commgraph/CommGraph.java
@@ -0,0 +1,19 @@
+package commgraph;
+
+import java.util.*;
+
+/**
+ * @author Ginny Dang
+ * 
+ * Detecting communities in a graph
+ */
+
+public class CommGraph {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
